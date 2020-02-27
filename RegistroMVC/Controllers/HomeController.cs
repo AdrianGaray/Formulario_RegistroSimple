95665395b4688ad935d85826be0c03a198dc32f5
Adrian Alejandro Garay <adriangaray396@gmail.com>
2020-02-27 15:33:47 -0300

    Add project files.

---
+++ RegistroMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistroMVC.Models;
+
+namespace RegistroMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(RegistroTBL obj)
+        {
+            // Verifica que las validaciones que tenemos en nuestras vistas realmente se esten cumpliendo
+            if (ModelState.IsValid)
+            {
+                // crear una variable para que nos pueda guardar los datos en la base de datos.
+                MiRegistroEntities db = new MiRegistroEntities();
+                db.RegistroTBLs.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+    }
+}
